1e6c6f0db83d3c02dad67bdd6647df9f761257fd
fatmaelhot <fatmaelhot289@gmail.com>
2024-05-08 00:27:49 +0300

    add api routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApplicationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Define routes for applications
+Route::get('/applications', [ApplicationController::class, 'index']);
+Route::post('/applications', [ApplicationController::class, 'store']);
+Route::get('/applications/{id}', [ApplicationController::class, 'show']);
+Route::put('/applications/{id}', [ApplicationController::class, 'update']);
+Route::delete('/applications/{id}', [ApplicationController::class, 'destroy']);
