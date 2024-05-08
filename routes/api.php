4ede1e60ff5b06d3c5daa07299ddec9ab2d0d821
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-05-08 15:29:39 +0300

    update  skill route

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
-Route::apiResource('/v1/skills', SkillController::class);
+Route::apiResource('skills', SkillController::class);
 
 // Define the route for retrieving all skills associated with a specific job post
 Route::get('/v1/jobs/{jobId}/skills', [JobPostSkillController::class, 'index']);
