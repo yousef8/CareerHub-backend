da1be9aeed6d2de528db261f2fe65b9600cad785
Steven Adel <stevenadel.dev@gmail.com>
2024-05-08 11:17:53 +0300

    Add Industry model, factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             JobPostSeeder::class,
+            IndustrySeeder::class
         ]);
     }
 }
