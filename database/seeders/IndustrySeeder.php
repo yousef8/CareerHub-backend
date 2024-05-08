19d11250047a5cbc7bc7f6261be1fa22f894c8f3
Steven Adel <stevenadel.dev@gmail.com>
2024-05-08 11:17:53 +0300

    Add Industry model, factory and seeder

---
+++ database/seeders/IndustrySeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use \App\Models\Industry;
+
+class IndustrySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Industry::factory(5)->create();
+    }
+}
