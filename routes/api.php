79b44dd91b9abd6b8f4524c95ccbdd5f17d9c1e3
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-05-08 15:51:30 +0300

    update jobpostskill route

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,4 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('users', UserController::class);
 Route::apiResource('skills', SkillController::class);
 
-// Define the route for retrieving all skills associated with a specific job post
-Route::get('/v1/jobs/{jobId}/skills', [JobPostSkillController::class, 'index']);
-
-// Define the route for attaching a skill to a specific job post
-Route::post('/v1/jobs/{jobId}/skills', [JobPostSkillController::class, 'store']);
-
-// Define the route for removing a specific skill from a specific job post
-Route::delete('/v1/jobs/{jobId}/skills/{skillId}', [JobPostSkillController::class, 'destroy']);
+Route::apiResource('jobs', JobPostSkillController::class)
