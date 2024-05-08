244492c92eb79e953f64491132424854af9bb43b
Steven Adel <stevenadel.dev@gmail.com>
2024-05-08 12:14:56 +0300

    Add api routes for industry

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\IndustryController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
+Route::apiResource('industries', IndustryController::class);
