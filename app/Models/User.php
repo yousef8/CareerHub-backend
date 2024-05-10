ee9403d3e058edd3f0f5027cbf81029636e8f9e3
Yousef Elsayed <yousef4511@outlook.com>
2024-05-10 23:57:47 +0300

    Install Sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Storage;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
