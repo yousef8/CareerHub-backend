4798f0593dbbad56c06f21db7043db907ac4ab69
Yousef Elsayed <yousef4511@outlook.com>
2024-05-14 20:59:11 +0300

    Add necessary configuration for deployment on render

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+echo "Running composer"
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Seeding Database..."
+php artisan db:seed
+
+
+echo "Linking Storage..."
+php artisan storage:link
